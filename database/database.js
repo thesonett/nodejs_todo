6b00bd1ccbefab6b2fe86347b5c310a7ebf7ca7e
thesonett <sonettjsaha@gmail.com>
2024-04-10 15:39:51 +0530

    adding connection host name

--- database/database.js
+++ database/database.js
@@ -3,5 +3,5 @@ import mongoose from "mongoose";
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: "todo_backend",
-    }).then(c => console.log("Database connected!")).catch(err => console.log(err));
+    }).then(c => console.log(`Database connected with ${c.connection.host}`)).catch(err => console.log(err));
 }
